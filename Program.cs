dc2976d20c4ab60e2cfa5bdc69705d94778d34c7
Ivan <evankirk@vk.com>
2022-11-02 19:05:42 +0500

    Создание метода Task1Solving

--- Program.cs
+++ Program.cs
@@ -2,9 +2,21 @@
 {
     public class Lab5
     {
+        static void Task1Solving()
+        {
+            float floatNumberBefore = float.NaN;
+
+            while (true)
+            {
+                string variable = Console.ReadLine();
+                if (variable == "q")
+                    return;
+            }
+        }
+
         public static void Main()
         {
-
+            
         }
     }
 }

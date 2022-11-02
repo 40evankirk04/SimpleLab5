6223aa71f6e2fb99fd3700dcff969727dbe33391
Ivan <evankirk@vk.com>
2022-11-02 19:11:22 +0500

    Добавлены все условия

--- Program.cs
+++ Program.cs
@@ -9,14 +9,27 @@
             while (true)
             {
                 string variable = Console.ReadLine();
-                if (variable == "q")
+
+                if (variable == "q") return;
+
+                if (int.TryParse(variable, out int intNumber))
+                {
+                    Console.WriteLine((char)intNumber);
                     return;
+                }
+
+                if (float.TryParse(variable, out float floatNumber))
+                {
+                    if (floatNumberBefore == floatNumber)
+                        return;
+                    floatNumberBefore = floatNumber;
+                }
             }
         }
 
         public static void Main()
         {
-            
+            Task1Solving();
         }
     }
 }
